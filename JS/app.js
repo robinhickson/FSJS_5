230b4006b3642be9a90e6596ee106c5fdc8251e6
robinhickson <rkhickson@gmail.com>
2021-03-05 11:53:57 +0100

    Prevalidation

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,63 @@
-// global variables
-let employees = [];
+/* Treehouse FSJS Techdegree
+ * Project 5 - Public API Requests
+ * app.js */
 
+ /*jshint esversion: 8*/
+
+
+// Global variables
+let employees = [];
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
 email, location, phone, dob &noinfo &nat=us,gb,ie`
-const gridContainer = document.querySelector(".grid-container");
-const overlay = document.querySelector(".overlay");
-const modalContainer = document.querySelector(".modal-content");
-const modal = document.querySelector(".modal");
-const modalClose = document.querySelector(".modal-close");
-const modalNext = document.querySelector(".modal-next");
-const modalPrevious = document.querySelector(".modal-previous");
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+
 const employeesNames = [];
 let index;
 
+//Create HTML elements
+const search = document.createElement("input");
+search.setAttribute("class", "searchBox");
+search.setAttribute("type", "text");
+search.setAttribute("id", "search");
+search.setAttribute("placeholder", "Search for employees");
 
+header.insertAdjacentElement('beforeend', search);
+
+const gridContainer = document.createElement("main");
+gridContainer.setAttribute("class", "grid-container");
+body.insertAdjacentElement(`beforeend`, gridContainer);
+
+const overlay = document.createElement("div");
+overlay.setAttribute("class", "overlay hidden");
+body.insertAdjacentElement(`beforeend`, overlay);
+
+const modal = document.createElement("div");
+modal.setAttribute("class", "modal");
+overlay.insertAdjacentElement('beforeend', modal);
+
+const modalClose = document.createElement("button");
+modalClose.setAttribute("class", "modal-close");
+modalClose.textContent = "X";
+
+const modalPrevious = document.createElement("button");
+modalPrevious.setAttribute("class", "modal-previous");
+modalPrevious.textContent = "<";
+
+const modalNext = document.createElement("button");
+modalNext.setAttribute("class", "modal-next");
+modalNext.textContent = ">";
+
+const modalContent = document.createElement("div");
+modalContent.setAttribute("class", "modal-content");
+
+modal.appendChild(modalClose);
+modal.appendChild(modalPrevious);
+modal.appendChild(modalContent);
+modal.appendChild(modalNext);
+
+
+//Fetch data from randomuser.me and parse results into function to create employee card
 fetch(urlAPI)
     .then(res => res.json())
     .then(res => res.results)
@@ -22,6 +66,7 @@ fetch(urlAPI)
 
 
 function displayEmployees(employeeData) {
+
     employees = employeeData;
     // store the employee HTML as we create it
     let employeeHTML = '';
@@ -33,23 +78,22 @@ function displayEmployees(employeeData) {
         let city = employee.location.city;
         let picture = employee.picture;
         employeeHTML += `
-<div class="card" id="${index}" data-index="${index}">
-<img class="avatar" src="${picture.large}" />
-<div class="text-container">
-<h2 class="name">${name.first} ${name.last}</h2>
-<p class="email">${email}</p>
-<p class="address">${city}</p>
-</div>
-</div>
-`
+            <div class="card" id="${index}" data-index="${index}">
+            <img class="avatar" src="${picture.large}" />
+            <div class="text-container">
+            <h2 class="name">${name.first} ${name.last}</h2>
+            <p class="email">${email}</p>
+            <p class="address">${city}</p>
+            </div>
+            </div>
+            `
     });
+    gridContainer.insertAdjacentHTML('beforeend', employeeHTML);
 
-    gridContainer.innerHTML = employeeHTML;
 }
 
-
+//create the modal view (same logic)
 function displayModal(index) {
-
     let {
         name,
         dob,
@@ -61,43 +105,41 @@ function displayModal(index) {
 
     let date = new Date(dob.date);
     const modalHTML = `
-    <img class="avatar" src="${picture.large}" />
-    <div class="text-container">
-<h2 class="name-modal">${name.first} ${name.last}</h2>
-<p class="email">${email}</p>
-<p class="address">${location.city}</p>
-<p class="address">${location.country}</p>
-<br/><hr/>
-<p class="address">${phone}</p>
-<p class="address">${location.street.number} ${location.street.name} ${location.city} ${location.postcode}</p>
+        <img class="avatar" src="${picture.large}" />
+        <div class="text-container">
+            <h2 class="name-modal">${name.first} ${name.last}</h2>
+            <p class="email">${email}</p>
+            <p class="address">${location.city}</p>
+            <p class="address">${location.country}</p>
+            <br/><hr/>
+            <p class="address">${phone}</p>
+            <p class="address">${location.street.number} ${location.street.name} ${location.city} ${location.postcode}</p>
 
-<p>Birthday:
-${date.getDate()}/${date.getMonth()}/${date.getFullYear()}</p>
-</div>
-`;
+            <p>Birthday:
+            ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}</p>
+        </div>
+        `;
     overlay.classList.remove("hidden");
-    modalContainer.innerHTML = modalHTML;
-
+    overlay.classList.add("show");
+    modalContent.innerHTML = modalHTML;
 
 }
 
-gridContainer.addEventListener('click', e => {
-
-    // make sure the click is not on the gridContainer itself
-    if (e.target !== gridContainer) {
-        // select the card element based on its proximity to actual element
-        const card = e.target.closest(".card");
+//  get selections for search or modal
+body.addEventListener('click', e => {
+    const card = e.target.closest(".card");
+    if (e.target === search) {
+        search.addEventListener('keyup', e => {
+            let searchValue = search.value.toLowerCase();
+            searchKeywords(searchValue);
+        })
+    } else if (card) {
+        // select the card element based on its proximity to actual element  
         index = parseInt(card.getAttribute('data-index'));
         displayModal(index);
-    }
-
-});
-
-modal.addEventListener('click', e => {
-
-    if (e.target === modalClose) {
-        console.log(index, typeof index);
+    } else if (e.target === modalClose) {
         overlay.classList.add("hidden");
+        overlay.classList.remove("show");
     } else if (e.target === modalNext) {
         if ((index + 1) < employees.length) {
             index += 1;
@@ -113,13 +155,6 @@ modal.addEventListener('click', e => {
 
 
 ///search functionality
-let search = document.getElementById('search');
-    search.addEventListener('keyup', e => {
-    let searchValue = search.value.toLowerCase();
-    searchKeywords(searchValue);
-})
-
-
 function searchKeywords(value) {
     const displayMatches = [];
     let cardsOff = document.querySelectorAll('.card');
@@ -135,7 +170,6 @@ function searchKeywords(value) {
 
     // iterate through employee names to check for user input text match
     employeesNames.forEach((match, index) => {
-
         if (match.toLowerCase().includes(value)) {
             displayMatches.push(employees[index]);
             displayMatches.forEach(cardOn => {
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # employee_directory
-Unit 8
+FSJS Project 5
 
 Directory with modal window and search functionality
+
+Uses Fetch to get data from server, and then manipulates that data to provide a rendered directory with cards, each representing one employee.
